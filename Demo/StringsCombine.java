de24d610d263a55c5a6e82c51dfe2871aa3e7685
Anurag Reddy <anuragreddy92@gmail.com>
2020-11-11 07:10:02 +0530

    my first class

---
+++ Demo/StringsCombine.java
@@ -0,0 +1,17 @@
+package anurag;
+
+public class StringsCombine {
+
+	public static void main(String[] args) {
+		String a= "abc";
+		String b= "def";
+		str_combine("anurag","reddy");
+		str_combine("srujan","reddy");
+		str_combine(a,b);
+
+	}
+	public static void str_combine(String a, String b) {
+		System.out.println(a+b);
+	}
+
+}
---
+++ Demo/TwoIntegersDifferences.java
@@ -0,0 +1,19 @@
+package anurag;
+
+public class TwoIntegersDifferences {
+
+	public static void main(String[] args) {
+		int a= 25;
+		int b= 5;
+		IntegersDifferences(a, b);
+	}
+		public static void IntegersDifferences(int a, int b) {
+		System.out.println("Sum of two integers "+(a+b));
+		System.out.println("Difference of two integers "+(a-b));
+		System.out.println("Product of two integers "+(a*b));
+		System.out.println("Average of two integers "+(a+b)/2);
+		System.out.println("Distance of two integers "+(a-b));
+
+		}
+
+}
