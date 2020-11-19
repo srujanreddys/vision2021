1d43b9eb73154eade387a5e3974fbd76dab9e9e6
Srujan <srujanreddy222@gmail.com>
2020-11-18 18:46:40 -0800

    Arrays

---
+++ TestJava/src/anurag/ArraysTest.java
@@ -0,0 +1,17 @@
+package anurag;
+
+public class ArraysTest {
+
+	public static void main(String[] args) {
+		int  array[] = {1,2,3,5,6,6,20,49,13};
+		String str[] = {"car","bus"};
+		
+ 		System.out.print(array.length);
+ 		System.out.print(array[0]);
+ 		System.out.print(array[1]);
+ 		System.out.print(array[2]);
+ 		for(int a =  array[]; array.length; a++) {
+ 			System.out.println(a);
+ 		}
+	}
+}
