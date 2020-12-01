eb57de910f91a4b871ed30ceeeb35b349eedefed
Anurag Reddy <anuragreddy92@gmail.com>
2020-12-01 07:49:47 +0530

    update

--- TestJava/src/classes/Account.java
+++ TestJava/src/classes/Account.java
@@ -3,28 +3,47 @@ package classes;
 public class Account {
 	
 	private int accountNumber ;
-	String name;
+	private String name;
 	private float balance;
 	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setBalance(float balance) {
+		this.balance = balance;
+	}
+	
+	
+	public void setName(String studentName) {
+		name=studentName;
+	}
+	public float getBalance() {
+		return balance;
+	}
 	public void setAccountNumber(int accountNo) {
 		accountNumber = accountNo;
 	}
 	
 	public void depositAmount(float amount) {
-		if(amount > 1500)
-			balance = amount;
+	//	if(amount >= 100 && amount <= 20000)
+			balance = balance + amount;
 	}
 	
-	public float getBalance() {
-		return balance;
-	}
-	
-	public void withDraw(float amount) {
-		if(balance - amount > 1500)
+	public void withDrawLimit(float amount) {
+		//if(balance - amount > 1500 && balance - amount <= 20000)
 			balance = balance - amount;
 	}
-	
-	
-	
+	public void transferBalance(float amount, Account transferaccount) {
+		this.withDrawLimit(amount);
+		transferaccount.depositAmount(amount);
+	}
+	public Account(int accountNumber,String name,float balance) {
+		this.accountNumber=accountNumber;
+		this.name = name;
+		this.balance = balance;
+	}
 	
 }
--- TestJava/src/classes/TestAccount.java
+++ TestJava/src/classes/TestAccount.java
@@ -3,21 +3,38 @@ package classes;
 public class TestAccount {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Account acc1 = new Account();
-		acc1.name = "Anurag";
-		acc1.depositAmount(1600);
-		System.out.println(acc1);
-		System.out.println(acc1.name);
-		System.out.println(acc1.getBalance());
+
+		Account account1 = new Account(1,"account1",1500);
+		//account1.depositAmount(1600);
+		account1.withDrawLimit(350);
+		System.out.println(account1);
+		System.out.println("Account Number = "+account1.getAccountNumber());
+		System.out.println("Name = "+account1.getName());
+		System.out.println("Balance = "+account1.getBalance());
+		System.out.println("");
 		
-		Account acc2 = new Account();
-		acc2.name = "Srujan";
-		acc2.depositAmount(100);
-		System.out.println(acc2);
-		System.out.println(acc2.name);
-		System.out.println(acc2.getBalance());
-		
+		Account account2 = new Account(2,"account2",1500);
+		account2.depositAmount(99);
+		System.out.println(account2);
+		System.out.println("Account Number = "+account2.getAccountNumber());
+		System.out.println("Name = "+account2.getName());
+		System.out.println("Balance = "+account2.getBalance());
+		System.out.println("");
+
+		Account account3 = new Account(3,"account3",2000);
+		account3.depositAmount(20001);
+		System.out.println(account3);
+		System.out.println("Account Number = "+account3.getAccountNumber());
+		System.out.println("Name = "+account3.getName());
+		System.out.println("Balance = "+account3.getBalance());
+		System.out.println("");
+
+		Account account4 = new Account(4,"account4",111);
+		account4.depositAmount(19000);
+		System.out.println(account4);
+		System.out.println("Account Number = "+account4.getAccountNumber());
+		System.out.println("Name = "+account4.getName());
+		System.out.println("Balance = "+account4.getBalance());
 	}
 
 }
